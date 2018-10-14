9915a9ff23a33f6d232218de353768080a5f2d71
jpatel0 <jaypatel9670@gmail.com>
2018-10-14 19:23:41 +0530

    2.13 FCM test

---
+++ app/src/main/java/com/zero/shareby/fcm/FirebaseMessaging.java
@@ -0,0 +1,51 @@
+package com.zero.shareby.fcm;
+
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.zero.shareby.R;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+public class FirebaseMessaging extends FirebaseMessagingService {
+    private static final String CHANNEL_ID="myChannel";
+    public FirebaseMessaging() {
+        super();
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d("inside","yes");
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setVibrate(new long[]{0,250,250,250});
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(22, mBuilder.build());
+
+        Log.d("dataChat",remoteMessage.getData().toString()+"\n"+remoteMessage.getNotification().getTitle());
+        try
+        {
+            Map<String, String> params = remoteMessage.getData();
+            JSONObject object = new JSONObject(params);
+            Log.d("JSON_OBJECT", object.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+    }
+}
