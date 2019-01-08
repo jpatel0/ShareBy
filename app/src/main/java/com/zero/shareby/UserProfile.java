ffdad228f15e26412b7fc44665ddeee126111e97
jpatel0 <jaypatel9670@gmail.com>
2019-01-08 21:15:05 +0530

    8.9 Changed ic_launcher test icon to official icon

--- app/src/main/java/com/zero/shareby/UserProfile.java
+++ app/src/main/java/com/zero/shareby/UserProfile.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
